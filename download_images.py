79eea1a859aabe9cfe726b670d173018c309db2c
louisabraham <louis.abraham@yahoo.fr>
2018-06-12 00:00:31 +0200

    fix wrong images

--- download_images.py
+++ download_images.py
@@ -6,6 +6,7 @@ PATH = 'assets/img'
 def download_file(url, destination):
     CHUNK_SIZE = 32768
     response = requests.get(url, stream=True)
+    assert response.status_code == 200
     if 'content-length' in response.headers:
         total = int(response.headers['content-length'])
     else:
@@ -18,8 +19,11 @@ def download_file(url, destination):
                     f.write(chunk)
                     pbar.update(len(chunk))
 
-URL = 'http://fit450.com/WEB_PAGES_AND_IMAGES/5BX%%20html%%20files/chart%s_files/image00%s.gif'
+URL1 = 'http://fit450.com/WEB_PAGES_AND_IMAGES/5BX%%20html%%20files/chart%s_files/image00%s.gif'
+URL2 = 'http://fit450.com/WEB_PAGES_AND_IMAGES/5BX%%20html%%20files/CHART%s_files/image00%s.gif'
+
 for chart in range(6):
+    URL = URL2 if chart else URL1
     for ex in range(5):
         url = URL % (chart + 1, ex + 3)
         destination = PATH + '/chart%s_ex%s.png' % (chart, ex)
